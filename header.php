73ab3bc6d764a21b5877b85bc7a0f0ce1887292b
Raphael Leite <raphaelleite.oliveira@gmail.com>
2017-02-07 18:29:44 -0200

    Created the menu bar

--- header.php
+++ header.php
@@ -5,3 +5,12 @@
     <title>Eliane Cabelos</title>
   </head>
   <body>
+
+    <!-- Menu Superior -->
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="contato.php">Contato</a></li>
+      </ul>
+    </nav>
