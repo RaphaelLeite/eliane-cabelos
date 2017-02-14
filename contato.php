411a291bd59b4f1d54519cff12a5d374cfd39fe4
Raphael Leite <raphaelleite.oliveira@gmail.com>
2017-02-14 11:08:42 -0200

    editing contato

--- contato.php
+++ contato.php
@@ -1,88 +1,22 @@
 <?php include("header.php") ?>
 
-<form class="" action="contact-process.php" method="post">
-  <table>
-    <tr>
-      <td>Nome</td>
-      <td><input type="text" name="nome" placeholder="Digite seu nome aqui:"></td>
-    </tr>
-    <tr>
-      <td>Email</td>
-      <td><input type="text" name="email" placeholder="Digite seu nome aqui:"></td>
-    </tr>
-    <tr>
-      <td>Mensagem</td>
-      <td><textarea name="mensagem" rows="8" cols="80"></textarea></td>
-    </tr>
-  </table>
-</form>
-
-<p>sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkja
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjass
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-  sfldsahflkhdsalkjfhslkdjhfkljasdhjkflhsdakljfhkjlasdhfkjlhsadkjlfhsdkljhflkjas
-</p>
+<div class="formulario-box">
+  <form class="formulario" action="contact-process.php" method="post">
+    <table>
+      <tr>
+        <td>Nome</td>
+        <td><input type="text" name="nome" placeholder="Digite seu nome aqui:"></td>
+      </tr>
+      <tr>
+        <td>Email</td>
+        <td><input type="text" name="email" placeholder="Digite seu nome aqui:"></td>
+      </tr>
+      <tr>
+        <td>Mensagem</td>
+        <td><textarea name="mensagem" rows="8" cols="80"></textarea></td>
+      </tr>
+    </table>
+  </form>
+</div>
 
 <?php include("footer.php") ?>
