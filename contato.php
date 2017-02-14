edcf02b2f4fe41996bb00c9ac28045434a92e3ad
Raphael Leite <raphaelleite.oliveira@gmail.com>
2017-02-14 03:39:33 -0200

    Started designing the contact form

--- contato.php
+++ contato.php
@@ -1,3 +1,13 @@
 <?php include("header.php") ?>
 
+<form class="" action="contact-process.php" method="post">
+  <table>
+    <tr>
+      <td>Nome</td>
+      <td><input type="text" name="nome" placeholder="Insira seu nome aqui:"></td>
+    </tr>
+  </table>
+
+</form>
+
 <?php include("footer.php") ?>
