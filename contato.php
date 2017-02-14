97273259cba9ceb7ba3655841c7ad5a9dc39e27f
Raphael Leite <raphaelleite.oliveira@gmail.com>
2017-02-14 03:40:17 -0200

    contact form minor change

--- contato.php
+++ contato.php
@@ -4,10 +4,17 @@
   <table>
     <tr>
       <td>Nome</td>
-      <td><input type="text" name="nome" placeholder="Insira seu nome aqui:"></td>
+      <td><input type="text" name="nome" placeholder="Digite seu nome aqui:"></td>
+    </tr>
+    <tr>
+      <td>Email</td>
+      <td><input type="text" name="email" placeholder="Digite seu nome aqui:"></td>
+    </tr>
+    <tr>
+      <td>Mensagem</td>
+      <td><textarea name="mensagem" rows="8" cols="80"></textarea></td>
     </tr>
   </table>
-
 </form>
 
 <?php include("footer.php") ?>
