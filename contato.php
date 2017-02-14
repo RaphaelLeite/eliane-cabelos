e9462edbd529dec7e41e9071c94fb9b3db5b1bd3
Raphael Leite <raphaelleite.oliveira@gmail.com>
2017-02-14 11:12:56 -0200

    editing contato

--- contato.php
+++ contato.php
@@ -5,14 +5,20 @@
     <table>
       <tr>
         <td>Nome</td>
+      </tr>
+      <tr>
         <td><input type="text" name="nome" placeholder="Digite seu nome aqui:"></td>
       </tr>
       <tr>
         <td>Email</td>
+      </tr>
+      <tr>
         <td><input type="text" name="email" placeholder="Digite seu nome aqui:"></td>
       </tr>
       <tr>
         <td>Mensagem</td>
+      </tr>
+      <tr>
         <td><textarea name="mensagem" rows="8" cols="80"></textarea></td>
       </tr>
     </table>
